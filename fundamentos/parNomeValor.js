f4963e620649550ed23d8d0945b06fd28e773f99
Matheus_f4t <ma.assuncao.doc@gmail.com>
2023-08-26 19:37:24 -0300

    Aula - par Nome/Valor

--- fundamentos/parNomeValor.js
+++ fundamentos/parNomeValor.js
@@ -1,3 +1,23 @@
-class teste {
+//par nome/valor
+const saudacao = "Opa"; // contexto léxico 1
 
+
+function exe() {
+    const saudacao = "Falaaa"; // contexto léxico 2
+    return saudacao;
 }
+
+// Objetos são grupos aninhados de pares nome/valor
+const cliente = {
+    nome: "Pedro",
+    idade: 32,
+    peso: 90,
+    endereco: {
+        logradouro: "Rua legal",
+        numero: 123
+    }
+}
+
+console.log(saudacao);
+console.log(exe());
+console.log(cliente);
