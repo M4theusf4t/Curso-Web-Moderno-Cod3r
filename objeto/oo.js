13992d66dd08f309b3834266f67bdd1f1deda061
Matheus_f4t <ma.assuncao.doc@gmail.com>
2023-10-21 19:11:42 -0300

    Introdução ao OO em JS.

---
+++ objeto/oo.js
@@ -0,0 +1,21 @@
+// Código não executável !!
+
+// Procedural
+processamento(valor1, valor2, valo3);
+
+// Orientação a Objeto (OO)
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+}
+objeto.processamento(); // Foco passou a ser o objeto
+
+//Principios importantes:
+// 1. abstracao
+// 2. encapsulamento
+// 3. herança(prototype)
+// 4. polimorfismo
